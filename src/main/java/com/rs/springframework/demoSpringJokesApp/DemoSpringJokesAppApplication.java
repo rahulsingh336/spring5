24478aa61db90a9a24c4960f4ab27fbd03c74ebc
Singh, Rahul <Rahul.Singh3@mastercard.com>
2018-01-19 10:19:04 +0530

    initial commit

---
+++ src/main/java/com/rs/springframework/demoSpringJokesApp/DemoSpringJokesAppApplication.java
@@ -0,0 +1,12 @@
+package com.rs.springframework.demoSpringJokesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJokesAppApplication.class, args);
+	}
+}
